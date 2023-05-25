9a00a91169d2f79392e3e21b6910f21ed8b31a9d
Darwin Cabarrubias <cabarrubiasdarwin98@gmail.com>
2023-05-25 14:53:43 +0800

    wip

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
     public function index()
     {
-        return view('admin.roles.index');
+        $roles = Role::query()->paginate(10);
+        return view('admin.roles.index', compact('roles'));
+    }
+
+    public function create()
+    {
+        return view('admin.roles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|unique:roles,name'
+        ]);
+
+        Role::create($data);
+
+        return redirect()->route('admin.roles.index');
     }
 }
--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,9 +1,46 @@
 <x-admin-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 w-full">
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex justify-end py-2 px-2">
+                    <a href="{{route('admin.roles.create')}}" type="button" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-500 text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition-all text-sm dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800">Create</a>
+                </div>
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Roles
+                    <div class="flex flex-col">
+                        <div class="-m-1.5 overflow-x-auto">
+                          <div class="p-1.5 min-w-full inline-block align-middle">
+                            <div class="border rounded-lg overflow-hidden dark:border-gray-700">
+                              <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                <thead>
+                                  <tr>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Name</th>
+                                    <th scope="col" class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase">Actions</th>
+
+                                  </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 dark:divide-gray-700 ">
+                                    @foreach ($roles as $role)
+                                      <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{{ $role->name }}</td>
+
+                                        <td class="text-right">
+                                            <div class="px-2">
+                                                <a href="" > Delete</a>
+                                                <a href=""> Edit</a>
+                                            </div>
+
+                                        </td>
+
+                                      </tr>
+                                    @endforeach
+
+                                </tbody>
+                              </table>
+                              {{ !! links()!!}}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                 </div>
             </div>
         </div>
